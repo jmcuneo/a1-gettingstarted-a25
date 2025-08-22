defb4e2aca348e40039aadada23ef73da5a3a517
Owen Hart <owenvhart@gmail.com>
2025-08-22 16:09:05 -0400

    in-class follow-along

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/app.css':
+      sendFile( response, 'app.css' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
