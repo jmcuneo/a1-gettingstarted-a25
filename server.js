ed5816270ddb94a0c39c0c329533f9e2de516a10
lwaaron2023 <alwaller@wpi.edu>
2025-08-23 14:14:51 -0400

    Added a file pertaining to the css styling of the index.html page (index.css). Added a file for the animation on the webpage (index.css). Began styling the home page with added styles in the index.css file. Modified the server.js to be able to take any url, interpret it being based in the same folder as the server.js is located, then try to find it and return the file (or fail to return and send a 404 error).

--- server.js
+++ server.js
@@ -2,23 +2,26 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
+
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
+  if(request.url === '/'){
+    //Handles special case of base url
+    sendFile(response, 'index.html');
+  }
+  else{
+    //Handles other cases
+    sendFile(response, './'+request.url); //Makes it so that the server bases the urls on the directory the server is running in, not the base directory of the device
   }
 })
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+const sendFile = ( response, filename ) => {
+
+    fs.readFile(filename, (err, content) => {
+      if (err) response.end( '404 Error: File Not Found' ); //If there is an error reading the file it sends a 404 error
+      response.end(content, 'utf-8')
+    })
+
+
 }
