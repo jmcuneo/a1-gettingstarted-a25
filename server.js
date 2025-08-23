8556de7aeb92394c2fc4392bec71ecf09cfb26fe
Arianna Xie <ariannaxie7@gmail.com>
2025-08-23 14:53:56 -0400

    Added about me information and styled page using CSS.

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const http = require('http'),
       fs   = require('fs'),
+      path = require('path'), // given by ChatGPT when debugging
       port = 3000
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      sendFile( response, 'index.html' )
-      break
+        sendFile( response, 'index.html' )
+        break
     case '/index.html':
-      sendFile( response, 'index.html' )
-      break
+        sendFile( response, 'index.html' )
+        break
+    case '/mystyles.css':
+        sendFile( response, 'mystyles.css' )
+        break
+    case '/myphoto.jpg':
+        sendImage( response, 'myphoto.jpg' )
+        break
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -21,4 +28,12 @@ const sendFile = function( response, filename ) {
    fs.readFile( filename, function( err, content ) {
      response.end( content, 'utf-8' )
    })
+}
+
+const sendImage = function( response, filename ) {
+    const fullPath = path.join(__dirname, filename);
+    fs.readFile( fullPath, function( err, content ) {
+        response.writeHead(200, { 'Content-Type': 'image/jpeg' });
+        response.end(content);
+    })
 }
