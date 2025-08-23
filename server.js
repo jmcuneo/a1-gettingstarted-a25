ec6ad17a90ca16448de6f2f9e91799b290f77ded
tlnguyen2009 <tienlucnguyen2009@gmail.com>
2025-08-23 16:54:50 -0400

    Changing to server.js and adding style.css
    + Add some more routes for server.js and add some writeHead(status) for easier tracking later
    
    + Add some basic CSS

--- server.js
+++ server.js
@@ -1,24 +1,42 @@
 const http = require('http'),
       fs   = require('fs'),
-      port = 3000
+      port = 3001
 
 const server = http.createServer( function( request,response ) {
   switch( request.url ) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
+      break
+    case '/style.css':
+      sendFile(response, 'style.css', 'text/css')
+      break
+    case '/public/Doodle_Art_Converted.jpg':
+      sendFile(response, 'public/Doodle_Art_Converted.jpg', 'image/jpeg') // be careful with relative path and absolute path, if we use "/" the file system FS later will understand we want to start from C:/ in the hard-disk
       break
     default:
+      response.writeHead(404); // if there is no writeHead, it will send default status as '200'
       response.end( '404 Error: File Not Found' )
   }
 })
 
-server.listen( process.env.PORT || port )
+server.listen( process.env.PORT || port, function(){
+  console.log("backend is running")
+})
 
-const sendFile = function( response, filename ) {
+//send content of the file and type of that content
+const sendFile = function( response, filename, contentType) {
    fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
+     if (err) {
+      response.writeHead(500);
+      response.end('Server Error');
+      console.log("server error");
+      return;
+     }
+
+     response.writeHead(200, {'Content-Type': contentType}); // sending status code and contentType in header of http 
+     response.end(content); // send content in the body of html
    })
 }
