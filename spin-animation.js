830b13809ef140509eb274a17b9cceeb1be9b771
tlnguyen2009 <tienlucnguyen2009@gmail.com>
2025-08-24 16:50:17 -0400

    change content in index.html

--- spin-animation.js
+++ spin-animation.js
@@ -1,3 +1,4 @@
+/*-----------logic for spinning profile picture-------------*/
 picture = document.querySelector('.avatar-container').querySelector('img');
 
 // when the mouse hover over the profile picture, add spinning class
@@ -9,3 +10,22 @@ picture.addEventListener('mouseover', function(){
 picture.addEventListener('mouseout', function(){
     picture.classList.remove('spin-effect');
 });
+
+
+/*----------- assign colors for pallet -------------*/
+const colors = [
+            '#3F1A17', // Dark Brown
+            '#A14D2F', // Rust Orange
+            '#E2C044', // Mustard Yellow
+            '#F1EDDD', // Beige
+            '#F9F9F9'  // Pale Grey
+];
+
+const colorPallets = document.querySelectorAll('.color-box'); //return an array
+
+//loop through each 'div' tag in the array above
+colorPallets.forEach((div, index) => {
+    div.style.backgroundColor = colors[index];
+});
+
+
