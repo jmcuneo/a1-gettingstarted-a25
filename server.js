a7ed115ca9772a085dab2f4873e16fa4f7cf641c
Matthew Franco <matthew.m.franco@gmail.com>
2025-08-24 18:01:02 -0400

    feat: serving images

--- server.js
+++ server.js
@@ -2,23 +2,42 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-const server = http.createServer( function( request,response ) {
+const server = http.createServer( function( request, response ) {
   switch( request.url ) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile( response, 'index.html', 'text/html' )
+      break
+    case '/styles.css':
+      sendFile( response, 'styles.css', 'text/css' )
+      break
+    case '/me.png':
+      sendFile( response, 'me.png', 'image/png' )
+      break
+    case '/githubIcon.png':
+      sendFile( response, 'githubIcon.png', 'image/png' )
+      break
+    case '/linkedinIcon.png':
+      sendFile( response, 'linkedinIcon.png', 'image/png' )
       break
     default:
+      response.writeHead(404)
       response.end( '404 Error: File Not Found' )
   }
 })
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
+const sendFile = function( response, filename, contentType ) {
    fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
+     if (err) {
+       response.writeHead(404)
+       response.end('404 Error: File Not Found')
+       return
+     }
+     response.writeHead(200, { 'Content-Type': contentType || 'application/octet-stream' })
+     response.end( content )
    })
 }
