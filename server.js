f858dce89ce3d832fbda1290502ab12a9233dcb3
Brendan Leu <brenleu8@gmail.com>
2025-08-24 20:20:25 -0400

    Make server serve directory and run formatter

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
+    case "/":
+      sendFile(response, "index.html");
+      break;
     default:
-      response.end( '404 Error: File Not Found' )
+      sendFile(response, `.${request.url}`);
   }
-})
+});
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
+console.log(`server available at http://localhost:${process.env.PORT || port}`);
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
-}
+const sendFile = function (response, filename) {
+  fs.readFile(filename, function (err, content) {
+    if (err) {
+      response.end("404 Error: File Not Found");
+    }
+    response.end(content, "utf-8");
+  });
+};
