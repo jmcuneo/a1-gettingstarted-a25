6b125239bfa12ae9fd1813d629bd17c6535de640
Antonia Junod <niajunod@gmail.com>
2025-08-25 20:23:32 -0400

    yes 2.0

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+    fs   = require('fs'),
+    port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
+const server = http.createServer(function(request, response) {
+    switch (request.url) {
+        case '/':
+        case '/index.html':
+            sendFile(response, 'index.html', 'text/html')
+            break
 
-    case '/style.css':
-        sendFile( response, 'style.css' )
-        break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+        case '/style.css':
+            sendFile(response, 'style.css', 'text/css')
+            break
+
+        case '/script.js':
+            sendFile(response, 'script.js', 'application/javascript')
+            break
+
+        default:
+            response.writeHead(404, { 'Content-Type': 'text/plain' })
+            response.end('404 Error: File Not Found')
+    }
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function(response, filename, contentType) {
+    fs.readFile(filename, function(err, content) {
+        if (err) {
+            response.writeHead(500, { 'Content-Type': 'text/plain' })
+            response.end('500 Internal Server Error')
+        } else {
+            response.writeHead(200, { 'Content-Type': contentType })
+            response.end(content, 'utf-8')
+        }
+    })
 }
