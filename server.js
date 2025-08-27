58bf8e94dc9aeda72f69978a4dcc63c0935abc25
JimmyMartella <jamesmartella@gmail.com>
2025-08-27 16:13:43 -0400

    Some Basic CSS Expirimentation

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-const server = http.createServer( function( request,response ) {
+const server = http.createServer( function( request, response ) {
   switch( request.url ) {
     case '/':
       sendFile( response, 'index.html' )
       break
+    case '/index':
+      sendFile( response, 'index.html' )
+      break
     case '/index.html':
       sendFile( response, 'index.html' )
+      break
+    case '/index.css':
+      sendFile( response, 'index.css')
       break
     default:
       response.end( '404 Error: File Not Found' )
