0a2b8b97fe2bb5918bab197fa4f43dd7acb0e66c
ElijahGray99 <123423522+ElijahGray99@users.noreply.github.com>
2025-08-28 12:10:38 -0400

    tried to add my first javascript animation.

--- test.js
+++ test.js
@@ -1,6 +1,17 @@
 
 
 /* when the cursor moves we trigger an event! */
-document.addEventListener("mousemove", (event) => {
 
+/* this finds the element from the document so we can manipulate it! */
+const tracker = document.querySelector('.tracker');
+
+
+/* this part of the program runs whenever the mouse moves. When it does, we update
+the cordinates of the tracker element. */
+document.addEventListener("mousemove", (event) => {
+    tracker.style.left = event.pageX+ 'px'; // set x coordinates.
+    tracker.style.top = event.pageY + 'px'; // set y coordinates.
+    //tracker.style.transform = `translate(${event.clientX}px, ${event.clientY}px)`;
 })
+
+
