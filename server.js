2619060d75642cd34bf67597877d14b0a63ad822
crnorris1 <crnorris@wpi.edu>
2025-08-29 17:56:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -24,13 +24,12 @@ server.listen( process.env.PORT || port )
 
 const sendFile = function(response, filename) {
   fs.readFile(filename, function(err, content) {
-    if (err) { //if fails
+    if (err) {
       response.writeHead(500);
-      response.end('Error');
-	}
-	else {
-      	let contentType = 'text/html'; 
-      	if (filename.endsWith('.css')) contentType = 'text/css';  //style.css was being read as a text/html file, so this overrides that
+      response.end('500 Error: Internal Server Error');
+    } else {
+      let contentType = 'text/html'; // default
+      if (filename.endsWith('.css')) contentType = 'text/css';
 
       response.writeHead(200, { 'Content-Type': contentType });
       response.end(content, 'utf-8');
