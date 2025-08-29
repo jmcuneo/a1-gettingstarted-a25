225060265a99aac94b202d7a302ba5465e3aeac3
crnorris1 <crnorris@wpi.edu>
2025-08-29 17:52:48 -0400

    Overrides text/html file as text/css

--- server.js
+++ server.js
@@ -1,46 +1,39 @@
+other server
+
 const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-const server = http.createServer( function( request,response ) {  //first response is request from user,  next is response back from server
-  
-	const url = "." + request.url; //find the url from the user request
-	
-	if (fs.existsSync(url)){ //does this file exist?
-
-		switch( url ) { 
-   			case '/':
-     				sendFile( response, 'index.html' )
-     		 		break
-   
-    			default:
-				sendFile(response, url); 
-  		}
-	}
-	else {
-		response.end("404: Error. File does not exist.");
-	}
-
+const server = http.createServer( function( request,response ) {
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    case '/style.css':
+      sendFile( response, 'style.css' )
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
+  }
 })
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-
-	let filetype = 'text/html';
-
-	if (filename.endsWith('.css')){
-		filetype = 'text/css';
+const sendFile = function(response, filename) {
+  fs.readFile(filename, function(err, content) {
+    if (err) { //if fails
+      response.writeHead(500);
+      response.end('Error');
 	}
-	else if (filename.endsWith('.jpg'){
-		filetype = 'image/jpeg';
-	}
-	else if (filename.endswith('.js'){
-		filetype = 'application/javascript';
-	}
+	else {
+      	let contentType = 'text/html'; 
+      	if (filename.endsWith('.css')) contentType = 'text/css';  //style.css was being read as a text/html file, so this overrides that
 
-
-     response.end( content, 'utf-8' );
-   })
-}
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(content, 'utf-8');
+    }
+  });
+};
