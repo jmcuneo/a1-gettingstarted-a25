3a91860b8caaef574995b3c3e86a082abd808fe8
Nicholas Dricoll <njdriscoll@wpi.edu>
2025-08-29 18:52:57 -0400

    Modifies .js to allow for img and pdf to work on website

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+    fs   = require('fs'),
+    path = require('path'),
+    port = 3000
 
+
+// This is where i implemneted code from chatgpt to alo for the displaying of
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+    let filename = request.url === '/' ? 'index.html' : request.url.substring(1)
+
+    fs.readFile(filename, function(err, content) {
+        if (err) {
+            response.statusCode = 404
+            response.end('404 Error: File Not Found')
+        } else {
+            response.end(content)
+        }
+    })
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
+
 
 const sendFile = function( response, filename ) {
    fs.readFile( filename, function( err, content ) {
