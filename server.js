1709b6907a955915e80c45c9b7959bddc62e41ff
crnorris1 <crnorris@wpi.edu>
2025-08-29 17:13:39 -0400

    Changed so that CSS file can config

--- server.js
+++ server.js
@@ -2,35 +2,31 @@ const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    case '/style.css':
-      sendFile( response, 'style.css' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+const server = http.createServer( function( request,response ) {  //first response is request from user,  next is response back from server
+  
+	const url = "." + request.url; //find the url from the user request
+	
+	if (fs.existsSync(url)){ //does this file exist?
+
+		switch( url ) { 
+   			case './':
+     				sendFile( response, 'index.html' )
+     		 		break
+   
+    			default:
+				sendFile(response, url); 
+  		}
+	}
+	else {
+		response.end("404: Error. File does not exist.");
+	}
+
 })
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function(response, filename) {
-  fs.readFile(filename, function(err, content) {
-    if (err) {
-      response.writeHead(500);
-      response.end('500 Error: Internal Server Error');
-    } else {
-      let contentType = 'text/html'; // default
-      if (filename.endsWith('.css')) contentType = 'text/css';
-
-      response.writeHead(200, { 'Content-Type': contentType });
-      response.end(content, 'utf-8');
-    }
-  });
-};
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     response.end( content, 'utf-8' )
+   })
+}
