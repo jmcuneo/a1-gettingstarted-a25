a99b8668d995b9eea16bdc1890750b6eb3f70587
alongo <amandaylongo@gmail.com>
2025-08-29 09:53:38 -0400

    Remove support for serving colorpallette.png in the server

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ const server = http.createServer( function( request,response ) {
       response.setHeader('Content-Type', 'image/jpeg')
       sendFileRaw( response, 'nails.jpeg' )
       break
-    case '/colorpallette.png':
-      response.setHeader('Content-Type', 'image/png')
-      sendFileRaw( response, 'colorpallette.png' )
-      break
     case '/app.js':
       response.setHeader('Content-Type', 'application/javascript')
       sendFile( response, 'app.js' )
