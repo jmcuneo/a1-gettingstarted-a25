af3d95250885035fb6c7c88df3514fee90990d50
crnorris1 <crnorris@wpi.edu>
2025-08-29 17:59:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-other server
-
 const http = require('http'),
       fs   = require('fs'),
       port = 3000
@@ -12,9 +10,6 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
-    case '/style.css':
-      sendFile( response, 'style.css' )
-      break
     default:
       response.end( '404 Error: File Not Found' )
   }
@@ -22,17 +17,8 @@ const server = http.createServer( function( request,response ) {
 
 server.listen( process.env.PORT || port )
 
-const sendFile = function(response, filename) {
-  fs.readFile(filename, function(err, content) {
-    if (err) {
-      response.writeHead(500);
-      response.end('500 Error: Internal Server Error');
-    } else {
-      let contentType = 'text/html'; // default
-      if (filename.endsWith('.css')) contentType = 'text/css';
-
-      response.writeHead(200, { 'Content-Type': contentType });
-      response.end(content, 'utf-8');
-    }
-  });
-};
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     response.end( content, 'utf-8' )
+   })
+}
