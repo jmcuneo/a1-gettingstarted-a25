cf86bd37cee9c6188db2e9f8a3191003cc8ed7bf
peczepiel <czepielpeterbusiness@gmail.com>
2025-08-29 12:32:55 -0400

    added image

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+const http = require('http');
+const fs   = require('fs');
+const port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
+const server = http.createServer((request, response) => {
+  switch (request.url) {
     case '/':
-      sendFile( response, 'index.html' )
-      break
     case '/index.html':
-      sendFile( response, 'index.html' )
-      break
+      sendFile(response, 'index.html');
+      break;
     case '/style.css':
-      sendFile( response, 'style.css', 'text/css' )
-      break
+      sendFile(response, 'style.css');
+      break;
     default:
-      response.end( '404 Error: File Not Found' )
+      response.statusCode = 404;
+      response.end('404 Error: File Not Found');
   }
-})
+});
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+function sendFile(response, filename) {
+  fs.readFile(filename, (err, content) => {
+    if (err) {
+      console.error('Error loading', filename, err.code);
+      response.statusCode = 500;
+      response.end('Server Error');
+    } else {
+      response.end(content);
+    }
+  });
 }
